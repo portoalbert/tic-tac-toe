a3ed21f958b11876cbe211816e0d8f6980551182
Albert Porto <albertporto@gmail.com>
2022-08-13 18:21:49 -0400

    new updates to UI and mechanics

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const markx = document.getElementById('exampleone')
-
-
-function test () {
-    markx.innerHTML = "X"
+const gamediv = document.getElementById('gamediv')
+let turn = 1
+console.log("playerone",turn)
+function marker (e) {
+    if (turn === 2){
+        e.target.innerText = "O"
+        turn = 1
+        return;
+    }
+    e.target.innerText = "X"
+    turn = 2
+    console.log("playerone",turn)
 }
+
+function boardMaker () {
+    
+    for (let i = 0; i < 9 ; i++){
+        const diver = document.createElement('div')
+        diver.addEventListener("click",marker);
+        diver.classList.add('square');
+        gamediv.appendChild(diver);
+    }   
+}
+boardMaker ();
